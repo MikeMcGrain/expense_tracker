016644309747a083bfec9ebce6c070cc6e6b1964
Mike McGrain <mcgrain.mike@gmail.com>
2020-03-11 16:56:57 -0500

    multiple components from an array

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react"
-import Form from "./Components/Form"
-import List from "./Components/List"
+// import Form from "./Components/Form"
+// import List from "./Components/List"
+import Product from "./Components/Product.js"
+import productsData from "./productsData.js"
 
 export default App => {
+  const productComponents = productsData.map(item => <Product key={item.id} product={item} />)
   return (
-    <>
-      <Form />
+    <div>
+      {/* <Form />
       <List catInfo={{name: "Mr. Whiskers", imageURL: "http://placekitten.com/300/200", alt: "cat picture"}} />
-      {/* <List 
+      <List 
         name="Catters"
         imageURL="http://placekitten.com/400/200"
-        alt ="another friggin' cat picture" /> */}
-    </>
+        alt ="another friggin' cat picture" />
+      <Product products={productsData} /> */}
+      {productComponents}
+      </div>
   )
 }
